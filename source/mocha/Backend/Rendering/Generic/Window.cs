4976547d9a5c5fe4f8d9c58799303dd0aa251ab1
Alex Guthrie <12881812+xezno@users.noreply.github.com>
2024-04-06 16:39:08 +0100

    Better Windowing code

--- source/mocha/Backend/Rendering/Generic/Window.cs
+++ source/mocha/Backend/Rendering/Generic/Window.cs
@@ -1,23 +1,35 @@
 ﻿using Mocha.Platforms;
+using Silk.NET.Core;
 using Silk.NET.Core.Contexts;
 using Silk.NET.Windowing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Mocha.Rendering;
 
 internal unsafe class Window : IDisposable
 {
+	[DllImport( "user32.dll", SetLastError = true )]
+	private static extern nint FindWindow( string lpClassName, string lpWindowName );
+	
+	private string _title = "My Window";
+	
 	private readonly int _width = 1280;
 	private readonly int _height = 720;
-	private readonly string _title = "My Window";
 	private IWindow? _window;
 
 	internal IVkSurface? VkSurface => _window?.VkSurface;
 	internal Vector2Int? FramebufferSize => _window?.FramebufferSize;
 	internal Action<double>? Render;
-	internal nint NativeHandle => _window?.Handle ?? nint.Zero;
+	internal nint NativeHandle => Process.GetCurrentProcess().MainWindowHandle; // what the FUCK
 	internal string Title => _window?.Title ?? "No title";
+	
+	public event Action? OnResize;
 
-	internal Window( CurrentPlatformInfo platform, string title, int width, int height )
+	internal Window( CurrentPlatformInfo platform, string title, int width, int height, string? icon = null )
 	{
 		_title = title;
 		_width = width;
@@ -25,7 +37,30 @@ internal unsafe class Window : IDisposable
 
 		InitWindow( platform );
 
+		if ( icon != null )
+		{
+			using var image = Image.Load<Rgba32>( icon );
+			var memoryGroup = image.GetPixelMemoryGroup();
+			
+			Memory<byte> array = new byte[memoryGroup.TotalLength * sizeof( Rgba32 )];
+			var block = MemoryMarshal.Cast<byte, Rgba32>( array.Span );
+
+			foreach ( var memory in memoryGroup )
+			{
+				memory.Span.CopyTo( block );
+				block = block[memory.Length..];
+			}
+
+			var iconImage = new RawImage( image.Width, image.Height, array );
+			_window!.SetWindowIcon( new ReadOnlySpan<RawImage>( iconImage ) );
+		}
+
 		_window!.Render += OnRender;
+
+		PerformanceStats.OnAverageCalculated += () =>
+		{
+			_window!.Title = $"{_title} - {PerformanceStats.AverageFPS} FPS ({(PerformanceStats.AverageDelta * 1000d):F3}ms)";
+		};
 	}
 
 	internal void OnRendererInit()
@@ -35,9 +70,20 @@ internal unsafe class Window : IDisposable
 		_window!.IsVisible = true;
 	}
 
+	private float t = 0;
+	
 	private void OnRender( double dt )
 	{
 		Render?.Invoke( dt );
+
+		Time.OnFrame( (float)dt );
+		PerformanceStats.OnFrame( dt );
+
+		if ( t >= 1f )
+		{
+			Log.Info( "Tick" );
+			t = 0;
+		}
 	}
 
 	internal void Run()
@@ -48,7 +94,7 @@ internal unsafe class Window : IDisposable
 	private void PlatformSpecificInit( CurrentPlatformInfo platform )
 	{
 		if ( platform.OperatingSystem == OperatingSystems.Windows )
-			Windows.InitWindow( this );
+			Windows.InitWindow( NativeHandle );
 	}
 
 	private WindowOptions GetWindowOptions( CurrentPlatformInfo platform )
@@ -72,15 +118,22 @@ internal unsafe class Window : IDisposable
 
 	private void InitWindow( CurrentPlatformInfo platform )
 	{
+		_title = GetWindowTitle( platform );
+		
 		var options = GetWindowOptions( platform ) with
 		{
 			Size = new Vector2Int( _width, _height ),
-			Title = GetWindowTitle( platform ),
+			Title = _title,
 			IsVisible = false
 		};
 
 		_window = Silk.NET.Windowing.Window.Create( options );
 		_window.Initialize();
+
+		_window.Resize += _ =>
+		{
+			OnResize?.Invoke();
+		};
 
 		PlatformSpecificInit( platform );
 	}

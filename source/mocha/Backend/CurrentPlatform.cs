62724a1ce49030ed57a3d6a7f44151a34a7c8a70
Alex Guthrie <12881812+xezno@users.noreply.github.com>
2024-04-06 16:10:33 +0100

    Uppercase everything in `CurrentPlatform`

--- source/mocha/Backend/CurrentPlatform.cs
+++ source/mocha/Backend/CurrentPlatform.cs
@@ -12,16 +12,16 @@ public enum OperatingSystems
 {
 	Windows,
 	Linux,
-	MacOS,
+	MacOs,
 	Other
 }
 
 public enum ArchitectureTypes
 {
-	x86,
-	x64,
-	ARM,
-	ARM64,
+	X86,
+	X64,
+	Arm,
+	Arm64,
 	Other
 }
 
@@ -40,7 +40,7 @@ public record struct CurrentPlatformInfo( Renderers Renderer, OperatingSystems O
 		else if ( RuntimeInformation.IsOSPlatform( OSPlatform.Linux ) )
 			return OperatingSystems.Linux;
 		else if ( RuntimeInformation.IsOSPlatform( OSPlatform.OSX ) )
-			return OperatingSystems.MacOS;
+			return OperatingSystems.MacOs;
 
 		return OperatingSystems.Other;
 	}
@@ -48,13 +48,13 @@ public record struct CurrentPlatformInfo( Renderers Renderer, OperatingSystems O
 	public static ArchitectureTypes GetArchitecture()
 	{
 		if ( RuntimeInformation.ProcessArchitecture == System.Runtime.InteropServices.Architecture.X86 )
-			return ArchitectureTypes.x86;
+			return ArchitectureTypes.X86;
 		else if ( RuntimeInformation.ProcessArchitecture == System.Runtime.InteropServices.Architecture.X64 )
-			return ArchitectureTypes.x64;
+			return ArchitectureTypes.X64;
 		else if ( RuntimeInformation.ProcessArchitecture == System.Runtime.InteropServices.Architecture.Arm )
-			return ArchitectureTypes.ARM;
+			return ArchitectureTypes.Arm;
 		else if ( RuntimeInformation.ProcessArchitecture == System.Runtime.InteropServices.Architecture.Arm64 )
-			return ArchitectureTypes.ARM64;
+			return ArchitectureTypes.Arm64;
 
 		return ArchitectureTypes.Other;
 	}
